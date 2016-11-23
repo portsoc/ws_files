6877c4390f4ec9aa96cea77d8d516104c9ba23b5
ear1grey <rjb@port.ac.uk>
2016-11-23 23:47:08 +0000

    initial tests

--- test.js
+++ test.js
@@ -4,204 +4,114 @@ var http = require('http');
 
 var dir = "./worksheet/";
 
-var pathUtil = "utility.js";
-var pathWeb = "webserver.js";
-var pathPkg = "package.json";
-
+var pathReverse = "reverse.js";
+var pathModule = "texttools.js";
+var pathReverseCandidate = "reverse_these.txt";
+var pathReverseOutput = "reverse_these_output.txt";
 
 
 
 /**
- * Create a file `webserver.js` within the worksheet folder.
+ * In this task you will create a reverse utiltiy which reverses
+ * all the characters in a text file.
  *
- * Reuse the code from the `http-add.js` example to implement
- * a web server that can listen on port 8080.
+ * Reuse the code from the `loadfile2.js` example to implement
+ * a utility called `reverse.js`.
  *
- * The server should respond to requests on `/subtract`, accepting
- * two  parameters, `a` and `b`, and returning the difference
- * between them as a plain text response.
- * e.g. '/subtract?a=2&b=3.4' should return -1.4
- * e.g. '/subtract?a=100&b=9' should return 91
- * e.g. '/subtract?b=300&a=200' should return -100
+ * The code that reverses the string should be placed in a module
+ * called `texttools.js`.  This should export a `reverse` function
+ * which takes one parameter, which is the string to be reversed,
+ * and returns the reversed string.
  *
- * If a path other than /subtract is requested a 404 error should be returned.
- *
- * Running the tests starts your web server, but if you want to try it in
- * your browser, you need to start the webserver explicitly, with the command
- * `node worksheet/webserver`
  */
 test(
-  "Create a file `" + pathWeb + "` in `" + dir + "`",
+  "`" + pathReverse + "` should exist in `" + dir + "`",
   function () {
     try {
-      fs.accessSync(dir+pathWeb, fs.F_OK);
-      ok(true, pathWeb + " created");
+      fs.accessSync(dir+pathReverse, fs.F_OK);
+
+      // so, did you notice?  This is the same code we used
+      // last week - and hey, look!... - it's using `fs.accessSync`
+      // to check whether the file exists :-)
+
+      ok(true, pathReverse + " created");
     } catch (e) {
-      ok(false, pathWeb + " is missing - please create it");
+      ok(false, pathReverse + " is missing - please create it");
+    }
+});
+
+test(
+  "`" + pathModule + "` should exist in `" + dir + "`",
+  function () {
+    try {
+      fs.accessSync(dir+pathModule, fs.F_OK);
+      ok(true, pathModule + " created");
+    } catch (e) {
+      ok(false, pathModule + " is missing - please create it");
     }
 });
 
 
+
 test(
-  "Subtract two numbers for the path /subtract",
+  "texttools should have a reverse function",
   function () {
-    require(dir+pathWeb);
-    var options = {
-      host: 'localhost',
-      port: '8080',
-      method: 'GET',
-      path: '/subtract?a=2&b=3.4',
-    };
+    var tools = require(dir+pathModule);
+    ok(
+      typeof tools.reverse === "function",
+      "Create a function called reverse."
+    );
+  }
+);
 
+test(
+  "the texttools reverse function should take one parameter",
+  function () {
+    var tools = require(dir+pathModule);
+    equal(
+      tools.reverse.length,
+      1,
+      "reverse should take exactly one parameter."
+    );
+  }
+);
+
+test(
+  "reversing tests",
+  function () {
+    var tools = require(dir+pathModule);
+    equal(tools.reverse(""), "", 'A reversed empty string should be an empty string');
+    equal(tools.reverse("1 2 3"), "3 2 1", 'numbers shoudl work fine');
+    equal(tools.reverse("hello"), "olleh", 'hello backwards is olleh');
+    equal(tools.reverse("hello world"), "dlrow olleh", 'hello world backwards is dlrow olleh');
+    equal(tools.reverse("hello world"), "dlrow olleh", 'hello world backwards is dlrow olleh');
+    equal(tools.reverse("1 2 3 four five six 7 8 9 0"), "0 9 8 7 xis evif ruof 3 2 1", 'mixed numbers and letters');
+  }
+);
+
+// once all these tests pass you know you have all
+// the functionality working, so now build a program
+// that uses it.
+
+
+test(
+  "run as an external program",
+  function () {
+
+    var exec = require('child_process').exec;
     stop();
+    exec(
+      "node " + dir+pathReverse + " " + dir+pathReverseCandidate + " " + dir+pathReverseOutput,
+      function(error, stdout, stderr) {
+        console.log(error, stderr);
 
-    var req = http.request(options, function(response) {
-      equal(response.statusCode, 200, 'successful /subtract should return status code 200');
-      var str = '';
-      response.on('data', function(chunk) { str += chunk; });
-      response.on('end', function() {
-        equal(str.trim(), '-1.4', 'calling /subtract?a=2&b=3.4 returns -1.4');
         start();
-      });
-    });
-    req.on('error', function (e) {
-      ok(false);
-      start();
-    });
-    req.end();
+        var data = fs.readFileSync(dir+pathReverseOutput, 'utf8');
+        equal(data,"\n0987654321\n0987654321", 'Reversed file contents are correct.');
+      }
+    );
+
+
+
   }
 );
-
-
-test(
-  "Return a 404 for all non-existent paths",
-  function () {
-    require(dir+pathWeb);
-    var options = {
-      host: 'localhost',
-      port: '8080',
-      method: 'GET',
-      path: '/nothere',
-    };
-
-    expect(1);
-    stop();
-
-    var req = http.request(options, function(response) {
-      equal(response.statusCode, 404, 'server should return 404 for /nothere');
-      start();
-    });
-    req.on('error', function (e) {
-      ok(false);
-      start();
-    });
-    req.end();
-  }
-);
-
-
-
-
-
-
-
-/**
- * Using `npm init`, create a `package.json` in the worksheet folder.
- * The name of the package should be worksheet and `main` should be `utility.js`.
- *
- * Create the file `utility.js` within the worksheet folder.
- * In that file export a function called `range` that takes one parameter,
- * an array of numbers. To implement the function, find the `underscore`
- * package and use its `max` and `min` functions to return the range of
- * the provided array, i.e. its maximal value minus its minimal value;
- * make sure to add the underscore package as a dependency in the top-level package.json
- *
- * Use the `npm install express --save` command in the `worksheet` directory
- * to install express, so your webserver above works.
- * Do a similar thing to install the underscore package.
- */
-test(
-  "Create a file `" + pathUtil + "`",
-  function () {
-    try {
-      fs.accessSync(dir+pathUtil, fs.F_OK);
-      ok(true, pathUtil + " created");
-    } catch (e) {
-      ok(false, pathUtil + " is missing - please create it");
-    }
-});
-
-
-test(
-  "Use `npm init` in `" + dir + "` to create a file `" + pathPkg + "`",
-  function () {
-    try {
-      fs.accessSync(dir+pathPkg, fs.F_OK);
-      ok(true, pathPkg + " created");
-    } catch (e) {
-      ok(false, pathPkg + " is missing - please create it");
-    }
-});
-
-
-test(
-  "Use the right name and main in " + pathPkg + ".",
-  function () {
-    var pkg = require('./worksheet/package.json');
-    equal(pkg.name, 'worksheet', "`worksheet/package.json` should have the name 'worksheet'.");
-    equal(pkg.main, 'utility.js', "`worksheet/package.json` should have 'main' set to 'utility.js'.");
-});
-
-
-test(
-  "Use the express and underscore packages.",
-  function () {
-    var pkg = require('./worksheet/package.json');
-    var deps = pkg.dependencies;
-    ok(deps, "`worksheet/package.json` should have dependencies.");
-    ok(deps && deps.express, "The express package should be a dependency in worksheet/package.json.");
-    ok(deps && deps.underscore, "The underscore package should be a dependency in worksheet/package.json.");
-
-    var express;
-    try {
-      express = require('./worksheet/node_modules/express');
-    } catch (e) {}
-    ok(express, 'express should be installed inside worksheet');
-
-    var _;
-    try {
-      _ = require('./worksheet/node_modules/underscore');
-    } catch (e) {}
-    ok(_, 'underscore should be installed inside worksheet');
-});
-
-
-test(
-  "Create a range function.",
-  function () {
-    var util = require(dir+pathUtil);
-    var _;
-    try {
-      _ = require('./worksheet/node_modules/underscore');
-    } catch (e) {
-      _ = require('underscore');
-    }
-
-    // instrument underscore min and max to count how often they are called
-    var oldMin = _.min;
-    var oldMax = _.max;
-    var minCount = 0;
-    var maxCount = 0;
-    _.min = function (arr) { minCount++; return oldMin(arr); }
-    _.max = function (arr) { maxCount++; return oldMax(arr); }
-
-    equal(util.range([4,3]), 1, "[4,3] has a range of 1");
-    equal(minCount, 1, "use underscore.min in your range function");
-    equal(maxCount, 1, "use underscore.max in your range function");
-    equal(util.range([3]), 0, "[3] has a range of 0");
-    equal(minCount, 2, "use underscore.min in your range function");
-    equal(maxCount, 2, "use underscore.max in your range function");
-    equal(util.range([3, -1, 5]), 6, "[3, -1, 5] has a range of 6");
-    equal(minCount, 3, "use underscore.min in your range function");
-    equal(maxCount, 3, "use underscore.max in your range function");
-});
