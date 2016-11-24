386bb78faffb4ef05643dbf164fa4c1ce1e3c11f
Jacek Kopecky <jacek@jacek.cz>
2016-11-24 00:21:29 +0000

    add mysql tests

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 'use strict';
 var fs = require('fs');
-var http = require('http');
+var mysql = require('mysql');
 
 var dir = "./worksheet/";
 
@@ -168,3 +168,199 @@ test(
 
   }
 );
+
+
+
+
+
+
+
+/**
+ * In the `worksheet` folder, make a module called `cardb.js`
+ * that implements a simple database of cars, as defined below.
+ *
+ * The module exports two functions:
+ *  1. `saveCar` with the following parameters: `reg`, `make`,
+ *     `model`, `year`, `price`, and `cb`. The function stores
+ *     the information (except `cb`) in a suitable database table
+ *     that you create. When it's done, the function must call `cb()`;
+ *     in case of an error, it should call `cb('error')` or with another
+ *     suitable error message.
+ *
+ *  2. `getAveragePrice` with two parameters: `year` and `cb`.
+ *     The function should find out the average price of all known cars
+ *     from the given year. When done, it will call `cb(null, price)` where
+ *     `price` is the computed average price. In case of an error (such as
+ *     that we don't have any cars from the given year) it should call
+ *     `cb('error')` or another error message to indicate that it failed.
+ *
+ * Put your database initialization SQL code in `worksheet/cardb_init.sql`.
+ *
+ * Put your database configuration in `worksheet/sql_config.json`.
+ * Inside the `mysql` object in `sql_config.json` you should have
+ * an extra property called `table` which contains the name
+ * of the database table where you store the information about cars,
+ * so that our tests can inspect that.
+ *
+ * PLEASE NOTE THAT OUR TESTS WILL DELETE ALL EXISTING DATA from the table
+ * of car information. For your testing, feel free to have an SQL script
+ * or a JS script that puts in some testing data.
+ */
+
+test(
+  "Create a file `worksheet/sql_config.json`",
+  function () {
+    try {
+      fs.accessSync('./worksheet/sql_config.json', fs.F_OK);
+      ok(true, "`worksheet/sql_config.json` created");
+    } catch (e) {
+      ok(false, "`worksheet/sql_config.json` is missing - please create it");
+    }
+
+    var config = require('./worksheet/sql_config.json');
+    ok(config.mysql.table, "`worksheet/sql_config.json` needs a property `table` within `mysql`");
+});
+
+
+test(
+  "Create a file `worksheet/cardb_init.sql`",
+  function () {
+    try {
+      fs.accessSync('./worksheet/cardb_init.sql', fs.F_OK);
+      ok(true, "`worksheet/cardb_init.sql` created");
+    } catch (e) {
+      ok(false, "`worksheet/cardb_init.sql` is missing - please create it");
+    }
+});
+
+
+test(
+  "Create a file `worksheet/cardb.js`",
+  function () {
+    try {
+      fs.accessSync('./worksheet/cardb.js', fs.F_OK);
+      ok(true, "`worksheet/cardb.js` created");
+    } catch (e) {
+      ok(false, "`worksheet/cardb.js` is missing - please create it");
+    }
+
+    var cardb = require('./worksheet/cardb');
+
+    ok(typeof cardb.saveCar === 'function', "cardb.js must export a function called `saveCar`");
+    ok(typeof cardb.getAveragePrice === 'function', "cardb.js must export a function called `getAveragePrice`");
+});
+
+
+test(
+  "Clear the database",
+  function () {
+    var config = require('./worksheet/sql_config.json');
+    var sql = mysql.createConnection(config.mysql);
+    expect(2);
+    stop();
+    sql.query(sql.format('select count(*) from ??', [config.mysql.table]), function (err, data) {
+      if (err) {
+        ok(false, 'Error checking that table ' + config.mysql.table + ' exists: ' + err);
+      } else {
+        ok('count(*)' in data[0], 'Table ' + config.mysql.table + ' exists');
+      }
+
+      sql.query(sql.format('delete from ??', [config.mysql.table]), function (err, data) {
+        if (err) {
+          ok(false, 'Error deleting data from table ' + config.mysql.table + ': ' + err);
+        } else {
+          ok(true, 'Table ' + config.mysql.table + ' cleared');
+        }
+        start();
+      });
+    });
+});
+
+
+test(
+  "saveCar",
+  function () {
+    var cardb = require('./worksheet/cardb');
+    var config = require('./worksheet/sql_config.json');
+    var sql = mysql.createConnection(config.mysql);
+    expect(9);
+    stop();
+    var timeout = setTimeout(function() {
+      ok(false, 'Timeout - are you calling the callbacks?');
+      start();
+    }, 3000);
+    cardb.saveCar('han 5010', 'Ford', 'Harrison', 1980, 8999.99, function (err) {
+      if (checkError('saving car', err)) return;
+
+    sql.query(sql.format('select count(*) from ??', [config.mysql.table]), function (err, data) {
+      if (checkError('checking count', err)) return;
+
+      equal(data[0]['count(*)'], 1, 'Expecting one car in the table');
+
+    cardb.saveCar('bn18 qqq', 'Brand', 'New', 2018, 15000, function (err) {
+      if (checkError('saving car', err)) return;
+
+    sql.query(sql.format('select count(*) from ??', [config.mysql.table]), function (err, data) {
+      if (checkError('checking count', err)) return;
+
+      equal(data[0]['count(*)'], 2, 'Expecting two cars in the table');
+
+    cardb.saveCar('abcd efg', 'Luxurius', 'Novus', 2018, 47000, function (err) {
+      if (checkError('saving car', err)) return;
+
+    sql.query(sql.format('select count(*) from ??', [config.mysql.table]), function (err, data) {
+      if (checkError('checking count', err)) return;
+
+      equal(data[0]['count(*)'], 3, 'Expecting three cars in the table');
+      start();
+      clearTimeout(timeout);
+    });
+    });
+    });
+    });
+    });
+    });
+});
+
+
+test(
+  "getAveragePrice",
+  function () {
+    // test average for one car
+    // test average for two cars
+    // test average for nonexistent year
+    var cardb = require('./worksheet/cardb');
+    expect(6);
+    stop();
+    var timeout = setTimeout(function() {
+      ok(false, 'Timeout - are you calling the callbacks?');
+      start();
+    }, 3000);
+    cardb.getAveragePrice(1980, function (err, avg) {
+      if (checkError('getting average', err)) return;
+      strictEqual(avg, 8999.99, 'the average price of cars from 1980 is 8999.99');
+
+    cardb.getAveragePrice(2018, function (err, avg) {
+      if (checkError('getting average', err)) return;
+      strictEqual(avg, 31000, 'the average price of cars from 2018 is 31k');
+
+    cardb.getAveragePrice(3000, function (err, avg) {
+      ok(err, 'no cars from 3000');
+      ok(avg == undefined, 'in case of error, must not return an average');
+      start();
+      clearTimeout(timeout);
+    });
+    });
+    });
+});
+
+
+
+function checkError(msg, err) {
+  if (err) {
+    ok(false, 'Error ' + msg + ': ' + err);
+    start();
+  } else {
+    ok(true, 'Success ' + msg);
+  }
+}
