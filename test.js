4c6022eea75b1f36f5221790faa7978c807afb7b
Jacek Kopecky <jacek@jacek.cz>
2016-12-08 13:03:03 +0000

    better docs for getAveragePrice

--- test.js
+++ test.js
@@ -399,7 +399,7 @@ test(
         strictEqual(avg, 31000, 'the average price of cars from 2018 is 31k');
 
         cardb.getAveragePrice(3000, function (err, avg) {
-          ok(err, 'no cars from 3000');
+          ok(err, 'no cars from 3000: if the query does not return anything, treat it as an error');
           ok(avg == undefined, 'in case of error, must not return an average');
 
           cardb.getAveragePrice('\'30', function (err, avg) {
